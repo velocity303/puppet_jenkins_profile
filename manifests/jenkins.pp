34f7504b7d3f486e9dc3bb02cc3d5b31c9d9a09f
James Jones <velocity303@gmail.com>
2014-10-24 17:56:10 +0000

    pulling down tomcat from source instead of package - initial test

--- manifests/jenkins.pp
+++ manifests/jenkins.pp
@@ -1,30 +1,24 @@
-class profile::jenkins ( 
-$version = 'latest',
-$catalina_base = '/var/lib/tomcat7',
-$catalina_home = '/usr/share/tomcat7',
-$tomcat_package = 'tomcat7',
-$tomcat_service = 'tomcat7',
-) {
-  class { 'tomcat':
-    catalina_home => $catalina_home,
+class profile::jenkins (
+    $version = 'latest',
+    $catalina_base = '/usr/share/tomcat',
+    $catalina_home = '/usr/share/tomcat',
+    $tomcat_package = 'tomcat6',
+    $tomcat_service = 'tomcat6',
+    ) {
+  class { 'java':
+    distribution => 'jre'
   }
-  tomcat::instance{ 'jenkins':
-    package_ensure      => present,
-    install_from_source => false,
-    package_name        => $tomcat_package,
+  class { 'tomcat':}
+  tomcat::instance{ 'default':
+    install_from_source => true,
+    source_url => "http://mirror.symnds.com/software/Apache/tomcat/tomcat-7/v7.0.56/bin/apache-tomcat-7.0.56.tar.gz",
   }->
   tomcat::setenv::entry { 'JENKINS_HOME':
-    value  => "\"-DJENKINS_HOME=${catalina_base}/webapps/jenkins\"",
-    param  => 'CATALINA_OPTS'
+    value       => "\"-DJENKINS_HOME=/opt/jenkins\"",
+    param       => 'CATALINA_OPTS',
   }->
   tomcat::war { 'jenkins.war' :
-    catalina_base => $catalina_base,
     war_source    => "http://mirrors.jenkins-ci.org/war/${version}/jenkins.war",
-    notify        => Service [ $tomcat_service ],
-  }
-  tomcat::service { 'jenkins': 
-    use_jsvc     => false,
-    use_init     => true,
-    service_name => $tomcat_service,
-  }
+  }->
+  tomcat::service { 'jenkins': }
 }
