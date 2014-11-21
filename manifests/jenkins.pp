b2f55e7d6563b04a3a301784752d01c7e440e061
James E. Jones <velocity303@gmail.com>
2014-11-21 01:02:38 -0600

    updated jenkins.pp

--- manifests/jenkins.pp
+++ manifests/jenkins.pp
@@ -1,16 +1,17 @@
 class profile::jenkins (
   $jenkins_version = 'latest',
-  $tomcat_major_version = '8',
-  $catalina_base = '/opt/apache-tomcat',
-  $catalina_home = '/opt/apache-tomcat',
+  $tomcat_major_version = '7',
+  $catalina_base = "/opt/apache-tomcat",
+  $catalina_home = "${catalina_base}",
+ 
 ) {
-  class { 'java':
+ class { 'java':
     distribution => 'jre'
   }
   case $tomcat_major_version {
     '6': { $tomcat_version = '6.0.41' }
-    '7': { $tomcat_version = '7.0.56' }
-    '8': { $tomcat_version = '8.0.14' }
+    '7': { $tomcat_version = '7.0.57' }
+    '8': { $tomcat_version = '8.0.15' }
   }
   class { 'tomcat':}
   firewall { '100 allow tomcat access':
@@ -21,19 +22,26 @@ class profile::jenkins (
   tomcat::instance{ 'default':
     install_from_source => true,
     source_url          => "http://www.us.apache.org/dist/tomcat/tomcat-${tomcat_major_version}/v${tomcat_version}/bin/apache-tomcat-${tomcat_version}.tar.gz",
-    catalina_base       => $catalina_base,
-    catalina_home       => $catalina_home,
-  }->
+    source_strip_first_dir => false,
+    catalina_base       => "${catalina_base}",
+    catalina_home       => "${catalina_home}",
+    before              => Tomcat::Setenv::Entry [ 'JENKINS_HOME' ],
+  }
   tomcat::setenv::entry { 'JENKINS_HOME':
     value               => "\"-DJENKINS_HOME=${catalina_base}/webapps/jenkins\"",
     param               => 'CATALINA_OPTS',
-  }->
-  tomcat::war { 'jenkins.war' :
+    before              => Tomcat::War [ "jenkins.war" ],
+  }
+  tomcat::war { "jenkins.war" :
     war_source    => "http://mirrors.jenkins-ci.org/war/${jenkins_version}/jenkins.war",
-    catalina_base => $catalina_base,
-  }->
-  tomcat::service { 'jenkins':
-    catalina_base => $catalina_base,
-    catalina_home => $catalina_home,
+    catalina_base => "${catalina_base}",
+    war_name      => "jenkins.war",
+    notify        => Tomcat::Service [ "jenkins" ],
+    subscribe     => Tomcat::Instance  [ 'default' ],
+  }
+  tomcat::service { "jenkins":
+    catalina_base => "${catalina_base}",
+    catalina_home => "${catalina_home}",
+    service_name  => "jenkins",
   }
 }
